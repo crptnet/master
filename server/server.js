c68922b3b530cfc5464b56af8ac69b12fc39fcfe
Pep <0962389013rom@gmail.com>
2023-04-21 00:01:25 +0300

    websocket

--- server/server.js
+++ server/server.js
@@ -15,15 +15,18 @@ const client = new MongoClient(uri, {
 
 
 const PORT = 5000
+async function connectDB(){
+  // Connect the client to the server	(optional starting in v4.7)
+  await client.connect();
+  // Send a ping to confirm a successful connection
+  await client.db("admin").command({ ping: 1 }).then(console.log("Connected to MongoDB!")).catch((e) => console.log("mongodb is not connected"))
+}
+
 async function run(){
     app.use('/api/coins', coinRouter)
     try {
         app.listen(PORT, () => console.log("Server started on port " + PORT))
         
-        // Connect the client to the server	(optional starting in v4.7)
-        await client.connect();
-        // Send a ping to confirm a successful connection
-        await client.db("admin").command({ ping: 1 }).then(console.log("Connected to MongoDB!")).catch((e) => console.log("mongodb is not connected"))
       } finally {
         // Ensures that the client will close when you finish/error
         await client.close();
---
+++ server/socket/coins.js
@@ -0,0 +1,2 @@
+const webSocket = require('ws')
+
