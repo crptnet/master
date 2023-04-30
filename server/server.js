149046627d257467845fa3af7d089096c5f93756
Pep <0962389013rom@gmail.com>
2023-05-01 01:47:46 +0300

    Removed unnecessary code

--- server/server.js
+++ server/server.js
@@ -1,13 +1,16 @@
 const express = require('express')
-const cors = require('cors')
 const app = express()
+const cors = require('cors')
 const env = require('dotenv').config()
 const connectToDb = require('./config/connectDB')
 const redisClient = require('./config/connectRedis')
 const errorHandler = require('./middleware/errorHandler')
-app.use(cors())
+const path = require('path');
+const { ConnectionPoolClosedEvent } = require('mongodb')
+
+
 app.use(errorHandler)
-app.use(express.static('public'));
+app.use(cors)
 
 connectToDb()
 
@@ -18,24 +21,6 @@ const PORT = 5000
 app.use(express.json());
 app.use('/api', require('./routes/userRouter'))
 app.use('/api', require('./routes/watchListRouter'))
-
-app.post('/upload', (req, res) => {
-    // Get the file that was set to our field named "image"
-    const { image } = req.files;
-
-    // If no image submitted, exit
-    if (!image) return res.sendStatus(400);
-
-    // If does not have image mime type prevent from uploading
-    if (/^image/.test(image.mimetype)) return res.sendStatus(400);
-
-    // Move the uploaded image to our upload folder
-    image.mv(__dirname + '/upload/' + image.name);
-
-    // All good
-    res.json().sendStatus(200);
-});
-
 
 
 app.listen(PORT, () =>{console.log('server started')})
