90c30c572589baa085d96777bce81aa937ffd393
Lordofaliens <93276741+Lordofaliens@users.noreply.github.com>
2023-05-01 01:44:57 +0300

    Add files via upload

--- server/server.js
+++ server/server.js
@@ -1,14 +1,13 @@
 const express = require('express')
+const cors = require('cors')
 const app = express()
 const env = require('dotenv').config()
 const connectToDb = require('./config/connectDB')
 const redisClient = require('./config/connectRedis')
 const errorHandler = require('./middleware/errorHandler')
-const path = require('path');
-const { ConnectionPoolClosedEvent } = require('mongodb')
-
-
+app.use(cors())
 app.use(errorHandler)
+app.use(express.static('public'));
 
 connectToDb()
 
@@ -19,6 +18,24 @@ const PORT = 5000
 app.use(express.json());
 app.use('/api', require('./routes/userRouter'))
 app.use('/api', require('./routes/watchListRouter'))
+
+app.post('/upload', (req, res) => {
+    // Get the file that was set to our field named "image"
+    const { image } = req.files;
+
+    // If no image submitted, exit
+    if (!image) return res.sendStatus(400);
+
+    // If does not have image mime type prevent from uploading
+    if (/^image/.test(image.mimetype)) return res.sendStatus(400);
+
+    // Move the uploaded image to our upload folder
+    image.mv(__dirname + '/upload/' + image.name);
+
+    // All good
+    res.json().sendStatus(200);
+});
+
 
 
 app.listen(PORT, () =>{console.log('server started')})
