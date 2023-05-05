b2d890b4ca0efb15321ee68c55af98c71de8ed1b
Vasich Roma <78172962+CharkPep@users.noreply.github.com>
2023-05-06 02:55:46 +0300

    Delete client directory

--- client/src/positions/positions.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './positions.css';
-import BTCPrice from './binanceCoins';
-import SideBar from '../sideBar';
-
-ReactDOM.createRoot(document.getElementById('sidebar')).render(<SideBar active={3} />);
-ReactDOM.createRoot(document.getElementById('main')).render(<BTCPrice />);
