6f748e8a26e777fee51c8b79ca53c4610d12003f
CharkPep <0962389013rom@gmail.com>
2023-05-28 21:02:13 +0300

    ++

--- client/src/index.js
+++ client/src/index.js
@@ -25,7 +25,7 @@ import GetListOfCoins from './listOfCoinsAPI';
 import APiManager from './components/APIManager/APIManager';
 
 //const serverLink = "http://localhost:5000/";
-const serverLink = "http://3.8.190.201.nip.io/";
+const serverLink = process.env.REACT_APP_LOCAL_DOMAIN || "http://3.8.190.201.nip.io/";
 
 const sidebarRoot = ReactDOM.createRoot(document.getElementById('sidebar'));
 const modelRoot = ReactDOM.createRoot(document.getElementById('model'));
@@ -33,7 +33,7 @@ const mainRoot = ReactDOM.createRoot(document.getElementById('main'));
 
 const pathName = window.location.pathname;
   console.log(pathName);
-  console.log(process.env.REACT_APP_LOCAL_DOMAIN)
+  console.log(serverLink)
   if (pathName.includes('/activate')) {
     mainRoot.render(<ActiveAccountPage location={window.location} />);
   } else if (pathName.includes('/reset-email')) {
