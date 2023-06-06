77a9ba3eb3e812f7a2f091be1d4e090b4688a195
CharkPep <0962389013rom@gmail.com>
2023-06-06 21:32:07 +0300

    Build

--- client/src/index.js
+++ client/src/index.js
@@ -35,7 +35,7 @@ import GetListOfCoins from './listOfCoinsAPI';
 import APiManager from './components/APIManager/KeyPairCreation/APIManager';
 import KeysTable from './components/APIManager/KeysTable/KeysTable'
 
-const serverLink = "http://localhost:5000/";
+const serverLink = process.env.REACT_APP_DOMAIN;
 
 const sidebarRoot = ReactDOM.createRoot(document.getElementById('sidebar'));
 const modelRoot = ReactDOM.createRoot(document.getElementById('model'));
